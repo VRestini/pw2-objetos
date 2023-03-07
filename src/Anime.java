48d76dd9edea193dfa7c64dbe3090cea83c35bee
Vitor Souza <vitorsouzalibrelonrestini@gmail.com>
2023-03-07 08:57:55 -0300

    Commit inicial

---
+++ src/Anime.java
@@ -0,0 +1,12 @@
+import java.time.LocalDate;
+
+public class Anime {
+    public int id;
+    public String nome;
+    public LocalDate dataLancamento;
+    public String classificacaoIndicativa;
+    public String genero;
+    public String autor;
+    public int quantidadeEpisodios;
+    public int temporadas;
+}
---
+++ src/AnimeTeste.java
@@ -0,0 +1,16 @@
+public class AnimeTeste {
+    public static void main(String[] args) {
+        //Class nomeObjeto = new Class();
+        Anime meuAnime = new Anime();
+        meuAnime.nome = "Vinland Saga";
+        meuAnime.quantidadeEpisodios = 32;
+        meuAnime.temporadas = 1;
+
+
+        Anime meuAnimePreferido = new Anime();
+        meuAnimePreferido.nome = "Dorohedoro";
+        meuAnime.quantidadeEpisodios = 327;
+        meuAnime.temporadas = 5;
+
+    }
+}
