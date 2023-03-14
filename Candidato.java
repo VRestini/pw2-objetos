98d9fdfe3a096595f8542e1754aa4bcb82b063d1
Vitor Souza <vitorsouzalibrelonrestini@gmail.com>
2023-03-14 07:45:16 -0300

    sla

---
+++ Candidato.java
@@ -0,0 +1,13 @@
+import java.time.LocalDate;
+
+public class Candidato {
+    public String nome;
+    public LocalDate dtnasc;
+    public String email;
+    public int celular;
+    public String escolaridade;
+    public String funcao;
+    public float salario;
+    public boolean empregado;
+    public boolean estagio;
+}
